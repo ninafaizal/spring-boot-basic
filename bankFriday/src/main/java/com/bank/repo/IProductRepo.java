f88bd6b2e11f2f52fc72609c448969386960563e
ninafaizal <ninafaizal@outlook.my>
2025-07-25 11:32:28 +0800

    Delete bankFriday directory

--- bankFriday/src/main/java/com/bank/repo/IProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.bank.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bank.entity.ProductEntity;
-
-public interface IProductRepo extends JpaRepository<ProductEntity, Long> {
-}
